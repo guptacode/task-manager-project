9f6c51d8c85723fc9d20916c9f0fbb55eaede2d2
V Gupta <vivgupta@me.com>
2023-08-23 10:57:40 +0100

    Create routes and controllers folder

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+
+app.use(express.static('./public'));
+app.use(express.json());
+
+app.get('/hello', (req, res) => {
+    res.send('Task Manager App');
+})
+
+app.use('/api/v1/tasks', tasks);
+
+app.listen(3000, () => {
+    console.log('Server is listening to port 3000...');
+})
---
+++ routes/tasks.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+router.route('/').get((req, res) => {
+    res.status(200).send('all items');
+});
+
+module.exports = router;
