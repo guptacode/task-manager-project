9d00b0e37dc9953b008969c0891dd3935ff3a29f
V Gupta <vivgupta@me.com>
2023-08-24 09:47:13 +0100

    Set up connection to the DB

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const tasks = require('./routes/tasks');
+require("dotenv").config();
+const connectDB = require("./db/connection");
+const tasks = require("./routes/tasks");
 
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 app.use(express.json());
 
-app.get('/hello', (req, res) => {
-    res.send('Task Manager App');
-})
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+});
 
-app.use('/api/v1/tasks', tasks);
+app.use("/api/v1/tasks", tasks);
 
-app.listen(3000, () => {
-    console.log('Server is listening to port 3000...');
-})
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(3000, () => {
+      console.log("Server is listening to port 3000...");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
